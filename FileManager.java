92ed665bcf301bd5fe7a4af5c1c6bc56fa8c902e
noaqe <uluk6556@gmail.com>
2026-04-22 10:24:22 +0600

    Update FileManager.java

--- FileManager.java
+++ FileManager.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 public class FileManager {
 
     public static void save(ArrayList<Student> list) {
-        try (ObjectOutputStream a = new ObjectOutputStream(new FileOutputStream("data.dat"))) {
+        try (ObjectOutputStream a = new ObjectOutputStream(new FileOutputStream("data.txt"))) {
             a.writeObject(list);
         } catch (Exception e) {
             System.out.println("Save error");
@@ -15,7 +15,7 @@ public class FileManager {
     }
 
     public static ArrayList<Student> load() {
-        try (ObjectInputStream a = new ObjectInputStream(new FileInputStream("data.dat"))) {
+        try (ObjectInputStream a = new ObjectInputStream(new FileInputStream("data.txt"))) {
             return (ArrayList<Student>) a.readObject();
         } catch (Exception e) {
             return new ArrayList<>();
